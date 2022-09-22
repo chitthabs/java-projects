ce1e5b01c0f1222883f004d8dadf779118a043ac
chittha b s <69895777+chitthabs@users.noreply.github.com>
2022-09-21 22:59:48 -0700

    Add files via upload

---
+++ HelloWorld2/src/HelloWorld/helloworld.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class helloworld {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10) {
+			System.out.printf("%d.Hello World!\n",i);
+			i++;
+			
+		}
+		
+
+	}
+}
+
+
